6aa046c941b7570255bd1a3966bcac1e572edd28
fgg12 <fgg12@FGG>
2015-06-02 23:17:46 +0300

    x

---
+++ src/Problem2/Matrix.java
@@ -0,0 +1,20 @@
+package Problem2;
+
+
+public class Matrix {
+    private int[][] matrix;
+    
+    public Matrix(){}
+    
+    public Matrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+}
